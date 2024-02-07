2f370da8fae0dc817ba8ad319864dcaee20eee64
MUHAMMADQASIM6936 <119961355+MUHAMMADQASIM6936@users.noreply.github.com>
2024-02-07 20:54:36 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar'
+import Hero from './component/Hero';
+function App() {
+
+  return (
+    <>
+  <Navbar/>
+  <Hero/>
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ index.js
@@ -0,0 +1,21 @@
+
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</StrictMode>
+);
+
+
+reportWebVitals();
